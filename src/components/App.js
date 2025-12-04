d67f0d0c2933cb8ba143dc254b777cc72fc8961e
Maryanne Kimani <maranne.kim@gmail.com>
2025-12-04 11:24:06 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,45 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
+import { useState } from "react";
 import TaskList from "./TaskList";
+import NewTaskForm from "./NewTaskForm";
+import CategoryFilter from "./CategoryFilter";
 
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+const CATEGORIES = ["All", "Code", "Food", "Money", "Misc"];
+const TASKS = [
+  { text: "Buy rice", category: "Food" },
+  { text: "Save a tenner", category: "Money" },
+  { text: "Build a todo app", category: "Code" },
+  { text: "Build todo API", category: "Code" },
+  { text: "Get an ISA", category: "Money" },
+  { text: "Cook rice", category: "Food" },
+  { text: "Tidy house", category: "Misc" },
+];
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleDeleteTask(text) {
+    setTasks(tasks.filter((task) => task.text !== text));
+  }
+
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
+  const visibleTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
+
   return (
     <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleAddTask} />
+      <TaskList tasks={visibleTasks} onDeleteTask={handleDeleteTask} />
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,43 @@
-import React from "react";
+import { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState(categories[1]); 
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onTaskFormSubmit({ text, category });
+    setText("");
+  }
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+    <form className="new-task-form" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="taskDetails">Details</label>
+        <input
+          id="taskDetails"
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="taskCategory">Category</label>
+        <select
+          id="taskCategory"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          {categories.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
         </select>
-      </label>
-      <input type="submit" value="Add task" />
+      </div>
+
+      <button type="submit">Add task</button>
     </form>
   );
 }
